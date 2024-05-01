9782a23f563c3cc1ffaf181e630630886d2780ce
gustavohnsv <guvi10vieira@gmail.com>
2024-04-30 21:11:24 -0300

    feat: added funcional buttons and style

--- src/components/Customer.tsx
+++ src/components/Customer.tsx
@@ -1,4 +1,5 @@
-import { FiTrash } from 'react-icons/fi';
+import { FiTrash, FiEdit3, FiActivity } from 'react-icons/fi';
+import { format } from 'date-fns'; // Function format(Date, 'mascara') -> Ex: format(new Date(), 'dd/MM/yyyy HH:mm')
 
 interface CustomerProps {
   id: string;
@@ -7,20 +8,46 @@ interface CustomerProps {
   phone: string;
   address: string;
   status: boolean;
+  create_at: Date;
+  update_at: Date;
 }
 
-export default function Customer({ customer, handleDelete }: { customer: CustomerProps, handleDelete: (id: string) => void }) {
-    return(
+export default function Customer({ customer, handleDelete, handleEdit, changeStatus }: { customer: CustomerProps, handleDelete: (id: string) => void , handleEdit: (id: string) => void, changeStatus: (id: string) => void}) {
+
+    const borderColor = customer.status ? 'border-lime-500' : 'border-gray-500';
+
+    return (
         <article 
-        key={ customer.id }
-        className="w-full bg-white rounded p-2 relative hover:scale-105 duration-200 shadow-md">
-            <p> <span className="font-medium"> Nome: </span> { customer.name } </p>
-            <p> <span className="font-medium"> Email: </span> { customer.email } </p>
-            <p> <span className="font-medium"> Telefone: </span> { customer.phone } </p>
-            <p> <span className="font-medium"> CEP: </span> { customer.address } </p>
-            <p> <span className="font-medium"> Status: </span> { customer.status ? 'ATIVO' : 'INATIVO' } </p>
-            <button className='bg-red-500 w-7 h-7 flex items-center justify-center rounded-lg absolute -top-2 right-0' onClick={() => handleDelete(customer.id)}>
-                <FiTrash size={18} color='#FFF'/>
+        key={customer.id}
+        className={`w-full bg-white rounded p-2 relative hover:scale-105 duration-200 shadow-md border-l-8 ${borderColor}`}
+        >
+            <p> <span className="font-medium"> Nome: </span> {customer.name} </p>
+            <p> <span className="font-medium"> Email: </span> {customer.email} </p>
+            <p> <span className="font-medium"> Telefone: </span> {customer.phone} </p>
+            <p> <span className="font-medium"> CEP: </span> {customer.address} </p>
+            <p> <span className="font-medium"> Criado em: </span> {format(customer.create_at, "dd/MM/yyyy HH:mm")} </p>
+            <p> <span className="font-medium"> Última mudança: </span> {format(customer.update_at, "dd/MM/yyyy HH:mm")} </p>
+            <p> <span className="font-medium"> Status: </span> {customer.status ? 'ATIVO' : 'INATIVO'} </p>
+            <button 
+            className='bg-red-500 w-6 h-6 flex items-center justify-center rounded-2xl absolute top-2 right-4 shadow-md' 
+            onClick={() => handleDelete(customer.id)} 
+            title='Remover'
+            >
+                <FiTrash size={16} color='#FFF' />
+            </button>
+            <button 
+            className='bg-yellow-500 w-6 h-6 flex items-center justify-center rounded-2xl absolute top-2 right-14 shadow-md' 
+            onClick={() => handleEdit(customer.id)}
+            title='Editar'
+            >
+                <FiEdit3 size={16} color='#FFF' />
+            </button>
+            <button 
+            className='bg-purple-500 w-6 h-6 flex items-center justify-center rounded-2xl absolute top-2 right-24 shadow-md' 
+            onClick={() => changeStatus(customer.id)}
+            title='Mudar status'
+            >
+                <FiActivity size={16} color='#FFF' />
             </button>
         </article>
     );
